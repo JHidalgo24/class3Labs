ae6c54943a3e3340a7f1232b1fdf5a293039365f
School Account <jhidalgo2@my.wctc.edu>
2021-03-31 09:08:29 -0500

    Calculator Done

--- pizza.js
+++ pizza.js
@@ -1,7 +1,8 @@
-function PizzaCalculation(){
+function pizzaCalculation(){
     let coworkerTotal = parseInt(prompt("How many of you are there including you?"))
     let toppingsAmount = parseInt(prompt("How many toppings are you getting?"))
 
-    let grandTotal = (15+(1.25*toppingsAmount));
-    alert(`Your grandtotal is $${grandTotal} split ${coworkerTotal} ways, you each owe $${grandTotal/coworkerTotal}`)
+    let grandTotal = (15+(1.25*toppingsAmount))
+    grandTotal = grandTotal.toFixed(2)
+    alert(`Your grandtotal is $${grandTotal} split ${coworkerTotal} ways, you each owe $${(grandTotal/coworkerTotal).toFixed(2)}`)
 }
